afa998875d8885b48600b33020749c58f9a7d5b6
Michiel Beijen <michiel.beijen@gmail.com>
2012-08-28 22:37:46 +0200

    Fixed dependencies in dist.ini

--- lib/App/OTRS/CreateTicket.pm
+++ lib/App/OTRS/CreateTicket.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package App::OTRS::CreateTicket;
 
-our $VERSION = '1.0.2';
+our $VERSION = '1.0.3';
 # ABSTRACT: Create tickets in OTRS via the Command Line Interface.
 
 1;
